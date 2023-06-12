1d443bccf418c5057b35f166b86c6c37366c914c
Esha Thakur <eshathakur888@gmail.com>
2023-06-12 15:55:52 +0530

    day1

--- Pascals_Triangle.cpp
+++ Pascals_Triangle.cpp
@@ -1,7 +1,35 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
+vector<vector<long long int>> printPascal(int n) 
+{
+  vector<vector<long long int>> result(n);
+  for(int i=0; i<n;i++){
+    result[i].resize(i+1);
+
+    result[i][0]=result[i][i]=1;
+
+    for(int j=1;j<i;j++){
+      result[i][j]=result[i-1][j-1]+result[i-1][j];
+    }
+  }
+
+  return result;
+}
+
 int main(){
-    
+    int n=4;
+    vector<vector<long long int>> res=printPascal(n);
+
+    int row=res.size();
+
+    for(int i=0;i<row;i++){
+        for(int j=0; j<=i;j++){
+            cout<<res[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
