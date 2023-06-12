096bd36bc4db0ef0fd5210f9c2e89e731eaf8d7c
Esha Thakur <eshathakur888@gmail.com>
2023-06-13 01:45:32 +0530

    q4

--- rotate_matrix.cpp
+++ rotate_matrix.cpp
@@ -1,59 +1,60 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-void rotateMatrix(vector<vector<int>> &mat, int n, int m)
+void rotateMatrix(vector<vector<int>>& mat, int n, int m)
 {
-    int left = 0 , right = m-1 , top  = 0 ,bottom = n-1;
+    int left = 0, right = m - 1, top = 0, bottom = n - 1;
 
-    while(left < right && top < bottom ){
+    while (left < right && top < bottom) {
         int temp = mat[top][left];
-        for(int col = left+1; col <= right; col++){
-            int k = mat[top][col];
-            mat[top][col] = temp;
-            temp = k;
+        
+        // Move elements from left to right
+        for (int col = left + 1; col <= right; col++) {
+            swap(mat[top][col], temp);
         }
         top++;
 
-        for(int row = top; row <= bottom; row++){
-            int k = mat[row][right];
-            mat[row][right] = temp;
-            temp = k;
+        // Move elements from top to bottom
+        for (int row = top; row <= bottom; row++) {
+            swap(mat[row][right], temp);
         }
         right--;
 
-        for(int col = right; col >= left; col--){
-            int k = mat[bottom][col];
-            mat[bottom][col] = temp;
-            temp = k;
+        // Move elements from right to left
+        for (int col = right; col >= left; col--) {
+            swap(mat[bottom][col], temp);
         }
         bottom--;
 
+        // Move elements from bottom to top
         for (int row = bottom; row >= top; row--) {
-          int k = mat[row][left];
-          mat[row][left] = temp;
-          temp = k;
+            swap(mat[row][left], temp);
         }
         left++;
 
         mat[top - 1][left - 1] = temp;
     }
     
-    for(int i=0; i<n;i++){
-        for(int j=0; j<m; j++){
-            cout<<mat[i][j]<<" ";
+    // Print the rotated matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << mat[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
 
-int main(){
+int main() {
     vector<vector<int>> matrix{
-        {1,2,3,4}, 
-        {5,6,7,8},
-        {9,10,11,12},
-        {13,14,15,16}
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 16}
     };
     
-    rotateMatrix(matrix,4,4);
+    rotateMatrix(matrix, 4, 4);
+    
     return 0;
 }
