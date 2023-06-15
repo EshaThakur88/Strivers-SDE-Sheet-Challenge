a82b87418d80cd7bd825e5c1f3d1287958e7cd13
Esha Thakur <eshathakur888@gmail.com>
2023-06-15 21:12:36 +0530

    q18

--- Modular_Exponentiation.cpp
+++ Modular_Exponentiation.cpp
@@ -5,7 +5,7 @@ int modularExponentiation(int x, int n, int m) {
 	int result=1;        
 	while(n>0){                
 		if(n&1){
-			//mean no is odd            
+			//mean no. is odd            
 			result = ((1LL * result * (x%m))%m);       
 		}                
 		x = (((long long)(x%m)  *  (x%m))  %m);        
