e8f6e3d3459c68f8cda05d486b9f18ead811d0b4
Esha Thakur <eshathakur888@gmail.com>
2023-07-02 20:40:05 +0530

    q46

---
+++ level_order_traversal.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+template <typename T>
+class BinaryTreeNode
+{
+public:
+    T val;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T val)
+    {
+        this->val = val;
+        left = NULL;
+        right = NULL;
+    }
+
+    vector<int> getLevelOrder(BinaryTreeNode<int> *root)
+    {
+
+        vector<int> ans;
+        if (root == NULL)
+            return ans;
+        queue<BinaryTreeNode<int> *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            int size = q.size();
+            for (int i = 0; i < size; i++)
+            {
+                BinaryTreeNode<int> *node = q.front();
+                q.pop();
+                if (node->left != NULL)
+                    q.push(node->left);
+                if (node->right != NULL)
+                    q.push(node->right);
+
+                ans.push_back(node->val);
+            }
+        }
+
+        return ans;
+    }
+};
